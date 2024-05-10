fb8810c707c0cd715d5ce2cb4dd4262f9b67b707
AlhandraM <110093031+AlhandraM@users.noreply.github.com>
2024-05-10 06:57:31 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import AccountContainer from "./AccountContainer";
+import AccountContainer from "./AccountContainer"; // Importing AccountContainer component
 
 function App() {
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
-        <h2>The Royal Bank of Flatiron</h2>
+        <h2>The Royal Bank of Flatiron</h2> {/* Title */}
       </div>
-      <AccountContainer />
+      <AccountContainer /> {/* Rendering AccountContainer component */}
     </div>
   );
 }
 
 export default App;
+
