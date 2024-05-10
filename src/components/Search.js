ceae420b371baaa6ebdf4447bff2fbd85d3cc6d6
AlhandraM <110093031+AlhandraM@users.noreply.github.com>
2024-05-10 07:04:53 -0400

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 function Search({ search, setSearch }) {
+  // Function to handle search input changes
   function handleSearch(event) {
     setSearch(event.target.value);
   }
 
   return (
     <div className="ui large fluid icon input">
+      {/* Search input field */}
       <input
         type="text"
         placeholder="Search your Recent Transactions"
         value={search}
-        onChange={handleSearch}
+        onChange={handleSearch} // Call handleSearch function on change
       />
+      {/* Search icon */}
       <i className="circular search link icon"></i>
     </div>
   );
 }
 
 export default Search;
+
